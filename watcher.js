4eb5dd76006ea2e9fbcc527460b729a10826693b
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-01-25 15:13:29 -0500

    Add handling for directory options. Fix validateLine return value.

--- watcher.js
+++ watcher.js
@@ -1,5 +1,31 @@
 const fs = require('fs');
+const readline = require('readline');
 const WatchFolder = require('./lib/watch_folder.js');
 
-const watcher = new WatchFolder('./input-directory');
+const options = {};
+const dataIn = process.argv;
+
+for (let i = 2; i < dataIn.length; i++) {
+  if (dataIn[i][0] === '-') {
+    options[dataIn[i].slice(1)] = dataIn[i + 1];
+    i ++
+  }
+}
+
+const watcher = new WatchFolder(options);
 watcher.start();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(`Watching...`)
+
+rl.on('line', line => {
+  if (line.toLowerCase() === 'exit') {
+    console.log('Exiting');
+    watcher.stop();
+    rl.close();
+  }
+});
